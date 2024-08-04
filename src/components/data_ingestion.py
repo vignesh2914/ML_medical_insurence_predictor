62a8906fa4b2be327ea48220c8768aa84cdb25b6
vignesh <vignesh2914@gmail.com>
2024-08-04 11:30:28 +0530

    Model_Trainer updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,8 +7,12 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerconfig
+from src.components.model_trainer import ModelTraining
 
 @dataclass
 class DataIngestionConfig:
@@ -49,5 +53,12 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
 
+    # transformation = DataTransformation()
+    # transformation.initiate_data_transformation(train_data, test_data)
+
+
     transformation = DataTransformation()
-    transformation.initiate_data_transformation(train_data, test_data)
+    train_arr,test_arr,_ = transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTraining()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,6 @@ import pickle
 from src.exception import CustomException
 
 from sklearn.metrics import r2_score
-from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path, obj):
@@ -20,3 +19,51 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+ 
+
+# def evaluate_models(X_train,y_train,X_test,y_test,model):
+#     try:
+#         report = {}
+
+#         for i in range(len(list(model))):
+#             model = list(model.values())[i]
+
+#             model.fit(X_train, y_train)
+
+#             y_train_pred = model.predict(X_train)
+
+#             y_test_pred = model.predict(X_test)
+
+#             train_model_score = r2_score(y_train, y_train_pred)
+
+#             test_model_score = r2_score(y_test, y_test_pred)
+
+#             report[list(model.keys())[i]] = test_model_score
+            
+#         return report
+    
+#     except Exception as e:
+#         raise CustomException(e, sys)
+
+        
